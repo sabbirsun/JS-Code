2cd07362009866bf1881aacf2b7268a7ce551f77
sabbirsun <sabbirsun1920@gmail.com>
2023-08-20 23:27:04 +0600

    Added Value

---
+++ hello.js
@@ -0,0 +1,30 @@
+// variable part Start//
+
+// console.log("Hello World");
+var friendsName = "MD. Sabbirul Islam";
+console.log(typeof friendsName);
+
+// variable part End//
+
+// String part Start//
+
+var strType = "MD. Sabbirul Islam Sun";
+console.log(typeof strType);
+
+// String part End//
+
+
+// Num part Start//
+
+var numType = 19201103109;
+console.log(typeof numType);
+
+// Num part End//
+
+// Boolean Type Variable part Start// True or False
+
+var BoolType = false;
+console.log(typeof BoolType);
+
+// Boolean Type Variable part End//
+
---
+++ mainprogram.js
@@ -0,0 +1,2 @@
+var rochona = "My name is MD. Sabbirul Islam Sun. My father's name is MD. Serajul Islam Sarker. My mother's name is Shahana Seraj.";
+console.log(rochona.toUpperCase());
