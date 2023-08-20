9dc57ee1857f602a394aa3ba0a46f42d2f4fdadc
sabbirsun <sabbirsun1920@gmail.com>
2023-08-21 00:41:38 +0600

    Final Added Value

--- mainprogram.js
+++ mainprogram.js
@@ -1,2 +1,5 @@
-var rochona = "My name is MD. Sabbirul Islam Sun. My father's name is MD. Serajul Islam Sarker. My mother's name is Shahana Seraj.";
-console.log(rochona.toUpperCase());
+// var rochona = "My name is MD. Sabbirul Islam Sun. My father's name is MD. Serajul Islam Sarker. My mother's name is Shahana Seraj.";
+// console.log(rochona.toUpperCase());
+
+var myFd = " Sabbir  Sun  Ruhul  Siam";
+console.log(myFd.split("Siam"));
