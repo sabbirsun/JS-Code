8bb9613c7dbbdde474680ae87f0bb7f2aae3308c
sabbirsun <sabbirsun1920@gmail.com>
2023-08-21 00:54:13 +0600

    IndexOf

--- mainprogram.js
+++ mainprogram.js
@@ -1,5 +1,6 @@
 // var rochona = "My name is MD. Sabbirul Islam Sun. My father's name is MD. Serajul Islam Sarker. My mother's name is Shahana Seraj.";
 // console.log(rochona.toUpperCase());
 
-var myFd = ("Sabbir Sun  Ruhul Siam");
-console.log(myFd.split("Siam"));
+var myFd = ("Sabbir Sun Ruhul Siam");
+//console.log(myFd.split("Siam"));
+console.log(myFd.indexOf("Sun"));
