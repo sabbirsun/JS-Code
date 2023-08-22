b34fbb356023008d842de4cd53b3c24345375936
sabbirsun <sabbirsun1920@gmail.com>
2023-08-22 19:12:32 +0600

    ParseFloat/ParseInt Added Value

--- mainprogram.js
+++ mainprogram.js
@@ -5,6 +5,9 @@ var myFd = ("Sabbir Sun Ruhul Siam");
 console.log(myFd.split("Siam"));
 //console.log(myFd.indexOf("Sun"));
 
-var num1 = 10;
+var num1 = "10";
 var num2 = 20;
+//console.log(num1 + num2);
+//num1 = parseFloat(num1);
+num1 = parseInt(num1); 
 console.log(num1 + num2);
