cd7a625995cd7b918cff83874b4942526219e573
sabbirsun <sabbirsun1920@gmail.com>
2023-08-22 19:33:28 +0600

    Bhagsesh Added Value

--- mainprogram.js
+++ mainprogram.js
@@ -1,13 +1,33 @@
 // var rochona = "My name is MD. Sabbirul Islam Sun. My father's name is MD. Serajul Islam Sarker. My mother's name is Shahana Seraj.";
 // console.log(rochona.toUpperCase());
 
-var myFd = ("Sabbir Sun Ruhul Siam");
-console.log(myFd.split("Siam"));
-//console.log(myFd.indexOf("Sun"));
+// var myFd = ("Sabbir Sun Ruhul Siam");
+// console.log(myFd.split("Siam"));
+// //console.log(myFd.indexOf("Sun"));
 
-var num1 = "10";
-var num2 = 20;
-//console.log(num1 + num2);
-//num1 = parseFloat(num1);
-num1 = parseInt(num1); 
-console.log(num1 + num2);
+// // ParseFloat/ParseInt Variable part Start//
+
+// var num1 = "10";
+// var num2 = 20;
+// //console.log(num1 + num2);
+// //num1 = parseFloat(num1);
+// num1 = parseInt(num1); 
+// console.log(num1 + num2);
+
+// var num3 = 10;
+// var num4 = 200;
+
+// //num4 = "" + num4; //String Type convert
+// console.log(num3 + num4);
+
+// // ParseFloat/ParseInt Variable part End//
+
+// Operators Variable part Start//
+
+var num5 = 550;
+var num6 = 600;
+//var total = num6 / num5; //Plus , Minus , Multiple , Divition
+var total = num5 % num6; //Bhagsesh
+console.log(total);
+
+// Operators Type Variable part End//
