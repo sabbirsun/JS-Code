446d8ae8ad7dda0623df09e45393ce80504b9b2c
sabbirsun <sabbirsun1920@gmail.com>
2023-08-22 19:59:13 +0600

    Predefined Function Added Value

--- mainprogram.js
+++ mainprogram.js
@@ -22,12 +22,37 @@
 
 // // ParseFloat/ParseInt Variable part End//
 
-// Operators Variable part Start//
+// // Operators Variable part Start//
 
-var num5 = 550;
-var num6 = 600;
-//var total = num6 / num5; //Plus , Minus , Multiple , Divition
-var total = num5 % num6; //Bhagsesh
-console.log(total);
+// var num5 = 550;
+// var num6 = 600;
+// //var total = num6 / num5; //Plus , Minus , Multiple , Divition
+// var total = num5 % num6; //Bhagsesh
+// console.log(total);
 
-// Operators Type Variable part End//
+// // Operators Type Variable part End//
+
+// // Predefined Function Type Variable part Start//
+
+//var num7 = -20;
+//var num8 = 10;
+
+//var sum = Math.abs(num7); //Negative value thakle sheita plus kore dei abs er maddhome//
+
+// var totalMembers = 30;
+
+// var myVote = 16.5;
+// //var myFd = 14;
+// var sum = Math.round(myVote); //0.5 er niche hole ager value bosbe r 0.5 er upore hole er porer vlaue bosbe//
+// console.log(sum);
+
+//var totalMembers = 20;
+//var myVote = 1.9;
+//var sum =Math.ceil(myVote); //0.1 bah je kono point ei thakuk na kno sheita tar porer value count korbe//
+//var sum =Math.floor(myVote); //0.1 bah je kono point ei thakuk na kno sheita tar nicher man er value count korbe//
+//console.log(sum);
+
+var lotary = Math.random() * 100;
+console.log(lotary); //0 theke 1 er moddhe bah ami joto gulor mosshe print korte chai eita ek ek bar ek ek value print korbe//
+
+// // Predefined Function Type Variable part End//
